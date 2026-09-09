d9910267e4a3ad20ec2c6a8a09eca1b7bcdea1c9
nathanbrownlee40-web <nathanbrownlee40@gmail.com>
2026-09-09 14:20:13 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -55,6 +55,7 @@ function render(){
  const s=totalStats();
  const hp=(id,val,cls)=>{const el=$(id);if(el){el.textContent=val;if(cls)el.className=cls}};
  hp("historyKpiBets",s.bets);hp("historyKpiWin",pct(s.win));hp("historyKpiStake",money(s.stake));hp("historyKpiReturns",money(s.ret));hp("historyKpiProfit",money(s.pl),s.pl>=0?"positive":"negative");hp("historyKpiRoi",pct(s.roi));
+ hp("dashKpiBets",s.bets);hp("dashKpiWin",pct(s.win));hp("dashKpiStake",money(s.stake));hp("dashKpiReturns",money(s.ret));hp("dashKpiProfit",money(s.pl),s.pl>=0?"positive":"negative");hp("dashKpiRoi",pct(s.roi));
  renderHistory();renderHistoryChart();renderDashLists();renderAnalytics();renderCharts();
 }
 function filtered(){
